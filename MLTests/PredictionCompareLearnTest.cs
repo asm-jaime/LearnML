4d974b361d4777f2bd2ebca7e9d7b1472d1f29db
asm-jaime <asm.jaime@gmail.com>
2022-11-09 02:27:58 +1000

    add quad error sample

---
+++ MLTests/PredictionCompareLearnTest.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace MLTests
+{
+    public class PredictionCompareLearnTest
+    {
+        [Test]
+        public void ShouldGetSimpleLearnError()
+        {
+            var weight = 0.5;
+            var input = 0.5;
+
+            var goal = 0.8;
+
+            var prediction = input * weight;
+
+            var error = Math.Round(Math.Pow(goal - prediction, 2), 2);
+            error.Should().Be(0.3);
+        }
+    }
+}
