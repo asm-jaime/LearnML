b9f280febe28c99fc9b17d44d706891cfe58f0dc
asm-jaime <asm.jaime@gmail.com>
2022-11-11 15:17:36 +1000

    add learning in n iterations

--- MLTests/PredictionCompareLearnTest.cs
+++ MLTests/PredictionCompareLearnTest.cs
@@ -22,6 +22,26 @@ namespace MLTests
             return result;
         }
 
+        public double LearnWeightInNIteration(double weight, double input, double predictionGoal, double step, int iterations)
+        {
+            var result = weight;
+            for(int iteration = 0; iteration < iterations; iteration++)
+            {
+                var prediction = input * result;
+                var error = Math.Pow(prediction - predictionGoal, 2);
+
+                var predictionUp = input * (result + step);
+                var errorUp = Math.Pow(predictionUp - predictionGoal , 2);
+
+                var predictionDn = input * (result - step);
+                var errorDn = Math.Pow(predictionDn - predictionGoal , 2);
+
+                if (errorDn < errorUp) result = result - step;
+                if (errorUp < errorDn) result = result + step;
+            }
+            return result;
+        }
+
         [Test]
         public void ShouldGetSimpleLearnError()
         {
@@ -66,5 +86,21 @@ namespace MLTests
             var betterError = Math.Round(Math.Pow(betterPrediction - goal, 2), 3);
             betterError.Should().Be(0.004);
         }
+
+        [Test]
+        public void ShouldIterativeLearn()
+        {
+            var weight = 0.5;
+            var input = 0.5;
+            var predictionGoal = 0.8;
+            var step = 0.001;
+            var iterations = 1101;
+
+            var goodWeight = LearnWeightInNIteration(weight, input, predictionGoal, step, iterations);
+            var result = Math.Round(input * goodWeight, 2);
+            result.Should().Be(0.8);
+        }
+
+
     }
 }
