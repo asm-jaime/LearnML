d4a2847b3aa461a8196031fd379f499a0adc5374
asm-jaime <asm.jaime@gmail.com>
2022-11-22 23:10:25 +1000

    add train on all data

--- MLTests/NStreetLightRecognizerTests.cs
+++ MLTests/NStreetLightRecognizerTests.cs
@@ -12,7 +12,7 @@ namespace MLTests
             for(int i = 0; i < iterations; i++)
             {
                 var prediction = input.dot(resultWeights);
-                var delta = prediction - predictionGoal ;
+                var delta = prediction - predictionGoal;
                 resultWeights = resultWeights - alpha * (input * delta);
             }
             return resultWeights;
@@ -44,6 +44,44 @@ namespace MLTests
             var weightsResult = NGradient(weights, input, predictionGoal, alpha, iterations);
             var result = (float) input.dot(weightsResult);
             result.Should().BeInRange(-0.0001f, 0.0001f);
+        }
+
+        [Test]
+        public void ShouldPredictBaseOnAllData()
+        {
+            var stop = 0f;
+            var walk = 1f;
+
+            var streetLights = np.array(new float[,] {
+                {1f, 0f, 1f},
+                {0f, 1f, 1f},
+                {0f, 0f, 1f},
+                {1f, 1f, 1f},
+                {0f, 1f, 1f},
+                {1f, 0f, 1f},
+            });
+            var stayOrWalk = np.array(new float[] { 0f, 1f, 0f, 1f, 1f, 0f });
+            var weights = np.array(new float[] { 0.5f, 0.48f, -0.7f });
+            var alpha = 0.1f;
+            var iterations = 1;
+
+            var input = streetLights[0];
+            var predictionGoal = stayOrWalk[0];
+            var trainedWeights = NGradient(weights, input, predictionGoal, alpha, iterations);
+
+            for(int i = 1; i <= 10; ++i)
+            {
+                for(int inputIndex = 0; inputIndex < stayOrWalk.len; ++inputIndex)
+                {
+                    input = streetLights[inputIndex];
+                    predictionGoal = stayOrWalk[inputIndex];
+                    trainedWeights = NGradient(trainedWeights, input, predictionGoal, alpha, iterations);
+                }
+            }
+
+            ((float)streetLights[0].dot(trainedWeights)).Should().BeInRange(-0.05f, 0.05f);
+            ((float)streetLights[1].dot(trainedWeights)).Should().BeInRange(0.90f, 1.1f);
+            ((float)streetLights[3].dot(trainedWeights)).Should().BeInRange(0.90f, 1.1f);
         }
     }
 }
