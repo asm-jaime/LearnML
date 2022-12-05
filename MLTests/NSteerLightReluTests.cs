9e709a4286839322b78f632c3ee3625e551adade
asm-jaime <asm.jaime@gmail.com>
2022-12-05 16:38:04 +1000

    add relu without type transformations

--- MLTests/NSteerLightReluTests.cs
+++ MLTests/NSteerLightReluTests.cs
@@ -1,4 +1,5 @@
-﻿using Numpy;
+﻿using FluentAssertions;
+using Numpy;
 using NUnit.Framework;
 
 namespace MLTests
@@ -71,6 +72,54 @@ namespace MLTests
             var resLayer0 = streetLights[$"{2}:{2 + 1}"];
             var resLayer1 = Relu(np.dot(resLayer0, weights01));
             var resLayer2 = np.dot(resLayer1, weights12);
+        }
+
+
+        [Test]
+        public void ShouldLearnByEmbeddedReluData()
+        {
+
+            var streetLights = np.array(new float[,] {
+                {1f, 0f, 1f},
+                {0f, 1f, 1f},
+                {0f, 0f, 1f},
+                {1f, 1f, 1f},
+            });
+            var stayOrWalk = np.array(new float[,] { { 1f, 1f, 0f, 0f } }).T;
+            var alpha = 0.2f;
+            var inputLayerSize = 3;
+            var hiddenLayerSize = 4;
+            var outputLayerSize = 1;
+
+            var weightsInputHidden = 2*np.random.rand(inputLayerSize, hiddenLayerSize) - 1;
+            var weightsHiddenOutput = 2*np.random.rand(hiddenLayerSize, outputLayerSize) - 1;
+
+            var iterations = 60;
+
+            for(int i = 1; i <= iterations; ++i)
+            {
+                for(int sampleId = 0; sampleId < streetLights.len; ++sampleId)
+                {
+                    var streetLight = streetLights[$"{sampleId}:{sampleId+1}"];
+                    var layerInput = streetLight;
+                    var layerHidden = np.dot(layerInput, weightsInputHidden);
+                    var layerHiddenRelu = layerHidden * (layerHidden > 0);
+                    var layerOutput = np.dot(layerHiddenRelu, weightsHiddenOutput);
+
+                    var deltaOutputHidden = stayOrWalk[$"{sampleId}:{sampleId + 1}"] - layerOutput;
+                    weightsHiddenOutput += alpha * layerHiddenRelu.T.dot(deltaOutputHidden);
+
+                    var layerHiddenReluMultiplayer = np.where(layerHiddenRelu > 0, np.array(1), np.array(0));
+                    var deltaInputHidden = deltaOutputHidden.dot(weightsHiddenOutput.T) * layerHiddenReluMultiplayer;
+                    weightsInputHidden += alpha * layerInput.T.dot(deltaInputHidden);
+                }
+            }
+
+            var inputLayer = streetLights[$"{2}:{2 + 1}"];
+            var hiddenLayer = np.dot(inputLayer, weightsInputHidden);
+            var outputLayer = np.dot(hiddenLayer * (hiddenLayer > 0), weightsHiddenOutput);
+            float data = outputLayer.GetData<float>().FirstOrDefault();
+            data.Should().BeInRange(-0.05f, 0.05f);
         }
     }
 }
