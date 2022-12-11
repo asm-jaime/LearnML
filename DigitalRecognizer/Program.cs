dc4f9c52e9475a43c7b480a0b7b25011ed7c9052
asm-jaime <asm.jaime@gmail.com>
2022-12-11 23:49:24 +1000

    add digital recognizer as consolas project

---
+++ DigitalRecognizer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace DigitalRecognizer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var recognizer = new NDLDigitalRecognizer(Path.GetFullPath(@"D:/projects.active/LearnML/data/mnist.npz"));
+            recognizer.TrainOnMNISTData();
+            var result = recognizer.PredictNumberFromImageTest(0);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
